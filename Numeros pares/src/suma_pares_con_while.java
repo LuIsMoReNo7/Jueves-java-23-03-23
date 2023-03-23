b27127a7a80758eea013d5f876ee8f62523030eb
LuIsMoReNo7 <1483853@senati.pe>
2023-03-23 10:51:44 -0500

    Usamo while e if para solucionar el problema

--- Numeros pares/src/suma_pares_con_while.java
+++ Numeros pares/src/suma_pares_con_while.java
@@ -1,17 +1,23 @@
 import java.util.Scanner;
-public class prueba_pares_con_while {
+public class suma_pares_con_while {
     public static void main(String[] args) {
-        /*Ingresamos los datos*/
+        /*Ingresamos el dato*/
         Scanner dato = new Scanner(System.in);
         System.out.println("ingrese el numero a mostrar: ");
         int n1 = dato.nextInt();
-        int i = 1;
+        //usamos int
+        int sum = 0;
+        int i = 0;
+        //llamamos a while para la condicion
         while(i<=n1){
             if (i % 2 == 0){
+              sum = sum + i;
             System.out.println("Numero par: "+i);
+          
             }
             i ++;
-    }
+      }
+      System.out.println("La suma es: "+sum); 
   }
 
 }
